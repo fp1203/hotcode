d6e5945119a90a40a0321bb1e14ff31b46881438
khotyn <hting1@gmail.com>
2013-07-03 16:45:02 +0800

    Add test cases

---
+++ src/test/cases/Base.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author khotyn 2013-07-03 10:18:08
+ */
+public class Base {
+
+    private static final String EXPECTED_RESULT          = "one";
+    private static final String EXPECTED_RELOADED_RESULT = "two";
+
+    public static void main(String[] args) throws Exception {
+        String caseName = args[0];
+        Class<?> klass = Thread.currentThread().getContextClassLoader().loadClass("A");
+        Method method = klass.getMethod("test");
+        String result = (String) method.invoke(klass.newInstance());
+        TimeUnit.SECONDS.sleep(1);
+        String reloadedResult = (String) method.invoke(klass.newInstance());
+
+        if (!EXPECTED_RESULT.equals(result) || !EXPECTED_RELOADED_RESULT.equals(reloadedResult)) {
+            System.err.println(caseName + " failed:");
+            System.err.println("Expected result before reloaded is " + EXPECTED_RESULT + ", but got "
+                               + (result == null ? "null" : result));
+            System.err.println("Expected result after reloaded is " + EXPECTED_RELOADED_RESULT + ", but got "
+                               + (reloadedResult == null ? "null" : reloadedResult));
+            System.exit(1);
+        }
+
+        System.out.println(caseName + " success!");
+    }
+}
--- test.sh
+++ test.sh
@@ -1,4 +1,45 @@
 #!/bin/sh
+function echo_red {
+   echo $'\e[31m'"$1"$'\e[00m'
+}
 
+function echo_green {
+   echo $'\e[32m'"$1"$'\e[00m'
+}
 
-echo "Hello, world!"
+CASE_BASE_DIR="src/test/cases"
+TARGET_BASE_DIR="target/cases"
+PROJ_DIR=`pwd`
+HOTCODE_AGENT_PATH="${PROJ_DIR}/target/hotcode.jar"
+FAILED="false"
+mkdir -p ${TARGET_BASE_DIR}
+cd ${CASE_BASE_DIR}
+
+for CASE in `find . -type d -d 1`; do
+    CASE_SOURCE_DIR="${PROJ_DIR}/${CASE_BASE_DIR}/${CASE}"
+    CASE_TARGET_DIR="${PROJ_DIR}/${TARGET_BASE_DIR}/${CASE}"
+    rm -r ${CASE_TARGET_DIR}
+    mkdir -p ${CASE_TARGET_DIR}
+    cp "${CASE_SOURCE_DIR}/A.java" "${CASE_TARGET_DIR}/A.java"
+    cd ${CASE_TARGET_DIR}
+    javac A.java
+    cp "${PROJ_DIR}/${CASE_BASE_DIR}/Base.java" "${CASE_TARGET_DIR}/Base.java"
+    cd ${CASE_TARGET_DIR}
+    javac Base.java
+    `java -javaagent:${HOTCODE_AGENT_PATH} -noverify Base ${CASE} > result &`
+    cp ${CASE_SOURCE_DIR}/A1.java ${CASE_TARGET_DIR}/A.java
+    javac A.java
+    sleep 0.5
+    RESULT=`cat ${CASE_TARGET_DIR}/result`
+    IS_SUCCESS=`echo ${RESULT} | grep success`
+    if [ -z "${IS_SUCCESS}" ]; then
+        echo_red "${RESULT}"
+        FAILED="true"
+    else
+        echo_green "${RESULT}"
+    fi
+done
+
+if [ "${FAILED}" == "true" ]; then
+    exit 1
+fi
